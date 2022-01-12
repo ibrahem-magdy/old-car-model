4c4869194adba04bea4eac14fa267bde13930bc5
ibrahem-magdy <hemamagdy752@gmail.com>
2022-01-12 22:12:09 +0200

    finish responsive

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
-import React, { useRef, useState, Suspense, useEffect } from "react";
-import { Canvas, useFrame, useThree, extend } from "@react-three/fiber";
+import React, { useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { SketchPicker } from "react-color";
 import { proxy, useSnapshot } from "valtio";
 
 import {
   Bounds,
-  useBounds,
   useGLTF,
   OrbitControls,
   ContactShadows,
-  Environment,
 } from "@react-three/drei";
 
 const state = proxy({
@@ -17,16 +15,14 @@ const state = proxy({
   items: {
     body: "#ffffff",
     wheels: "#ffffff",
-    glasses: "#ffffff",
+    glass: "#ffffff",
     rubber: "#ffffff",
-    number: "#ffffff",
-    light: "#ffffff",
+    numbers: "#ffffff",
+    lights: "#ffffff",
     // lighting: 0.5,
     // zooming: true,
   },
 });
-
-console.log(state.current);
 
 const Car = ({ ...props }) => {
   const group = useRef();
@@ -75,7 +71,7 @@ const Car = ({ ...props }) => {
               <mesh
                 geometry={nodes.car_body_body_0001_2.geometry}
                 material={materials["glass.001"]}
-                material-color={snap.items.glasses}
+                material-color={snap.items.glass}
                 receiveShadow
                 castShadow
               />
@@ -110,14 +106,14 @@ const Car = ({ ...props }) => {
               <mesh
                 geometry={nodes.car_body_body_0001_7.geometry}
                 material={materials["lights.001"]}
-                material-color={snap.items.light}
+                material-color={snap.items.lights}
                 receiveShadow
                 castShadow
               />
               <mesh
                 geometry={nodes.car_body_body_0001_8.geometry}
                 material={materials["number.001"]}
-                material-color={snap.items.number}
+                material-color={snap.items.numbers}
                 receiveShadow
                 castShadow
               />
@@ -174,78 +170,87 @@ function Picker() {
 }
 
 export default function App() {
-  const parts = ["body", "wheels", "light", "number", "rubber", "glasses"];
+  const parts = ["body", "wheels", "lights", "numbers", "rubber", "glass"];
 
   return (
     <>
-      {/* <h3 className="intro_header">change the color of car</h3> */}
+      <div className="con-error">
+        <img src="/error.png" alt="" />
+        <h3>Something went wrong.</h3>
+        <p className="sorry">
+          sorry,this site can only be open in the large screens
+        </p>
+      </div>
 
-      <p className="copyright">Developed by ibrahem magdy</p>
+      <div className="con-canvas">
+        {/* <h3 className="intro_header">change the color of car</h3> */}
 
-      <div className="controls">
-        <h3>change the colors of car </h3>
-        <div className="colors">
-          {parts.map((e) => {
-            return (
-              <div
-                key={Math.random()}
-                onClick={() => {
-                  state.current = e;
-                  console.log(state.current);
-                }}
-              >
-                {e}
-              </div>
-            );
-          })}
+        <p className="copyright">Developed by ibrahem magdy</p>
+
+        <div className="controls">
+          <h3>change the colors of car </h3>
+          <div className="colors">
+            {parts.map((e) => {
+              return (
+                <div
+                  key={Math.random()}
+                  onClick={() => {
+                    state.current = e;
+                  }}
+                >
+                  {e}
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
 
-      <div onClick={() => (state.current = null)}>
-        <Canvas
-          style={{ width: "100%", height: "100vh" }}
-          camera={{
-            position: [0, 500, 1000],
-            rotation: [0, (4 / 180) * Math.PI, 0],
-            fov: 50,
-          }}
-          dpr={[1, 2]}
-          shadows
-        >
-          <ambientLight intensity={0.7} />
-          <spotLight
-            intensity={0.5}
-            angle={0.1}
-            penumbra={1}
-            position={[10, 15, 10]}
-            castShadow
-          />
-          <directionalLight intensity={0.5} />
-          <Suspense fallback={null}>
-            {/* <CameraControls /> */}
+        <div onClick={() => (state.current = null)}>
+          <Canvas
+            style={{ width: "100%", height: "100vh" }}
+            camera={{
+              position: [0, 500, 1000],
+              rotation: [0, (4 / 180) * Math.PI, 0],
+              fov: 50,
+            }}
+            dpr={[1, 2]}
+            shadows
+          >
+            <ambientLight intensity={0.7} />
+            <spotLight
+              intensity={0.5}
+              angle={0.1}
+              penumbra={1}
+              position={[10, 15, 10]}
+              castShadow
+            />
+            <directionalLight intensity={0.5} />
+            <Suspense fallback={null}>
+              {/* <CameraControls /> */}
 
-            <Bounds fit clip margin={1.2}>
-              <Car rotation={[0, 0, 0]} />
-              <ContactShadows
-                rotation-x={Math.PI / 2}
-                position={[0, -0.8, 0]}
-                opacity={0.25}
-                width={10}
-                height={10}
-                blur={1.5}
-                far={0.8}
-              />
-            </Bounds>
-          </Suspense>
-          <OrbitControls
-            makeDefault
-            minPolarAngle={Math.PI / 3}
-            maxPolarAngle={Math.PI / 2}
-            enableZoom={false}
-          />
-        </Canvas>
+              <Bounds fit clip margin={1.2}>
+                <Car rotation={[0, 0, 0]} />
+                <ContactShadows
+                  rotation-x={Math.PI / 2}
+                  position={[0, -0.8, 0]}
+                  opacity={0.25}
+                  width={10}
+                  height={10}
+                  blur={1.5}
+                  far={0.8}
+                />
+              </Bounds>
+            </Suspense>
+            <OrbitControls
+              makeDefault
+              minPolarAngle={Math.PI / 3}
+              maxPolarAngle={Math.PI / 2}
+              enableZoom={false}
+            />
+          </Canvas>
+        </div>
+        <Picker />
       </div>
-      <Picker />
     </>
   );
 }
